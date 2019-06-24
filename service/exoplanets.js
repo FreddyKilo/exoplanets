35575081e47da674bd00d4395f77bc4cf896b249
Freddy <freddy.kilo@gmail.com>
2019-06-23 22:35:40 -0700

    Add timeline functionality

--- service/exoplanets.js
+++ service/exoplanets.js
@@ -1,16 +1,32 @@
 const _ = require("lodash");
 
-function getMockData(){
+const planetTimeline = [];
+
+function getMockData() {
     return {'testPlanetName': 'korg161'};
 }
 
+/**
+ *
+ * @param planetData
+ * @returns {{planetWithHottestStarName: *, orphanPlanetCount: number}}
+ */
 function getFilteredExoplanetData(planetData) {
     const orphanPlanetCount = getOrphanPlanetCount(planetData);
     const planetWithHottestStar = getPlanetWithHottestStar(planetData);
-    return {orphanPlanetCount: orphanPlanetCount,
-    planetWithHottestStarName: planetWithHottestStar.PlanetIdentifier}
+    const timeLine = getPlanetTimeline(planetData);
+    return {
+        orphanPlanetCount: orphanPlanetCount,
+        planetNameWithHottestStar: planetWithHottestStar.PlanetIdentifier,
+        timeLine: timeLine
+    }
 }
 
+/**
+ *
+ * @param planetData
+ * @returns {number}
+ */
 function getOrphanPlanetCount(planetData) {
     // filter planets where TypeFlag = 3 (no star)
     const filteredPlanets = _.filter(planetData, function (planet) {
@@ -20,6 +36,10 @@ function getOrphanPlanetCount(planetData) {
     return filteredPlanets.length;
 }
 
+/**
+ *
+ * @param planetData
+ */
 function getPlanetWithHottestStar(planetData) {
     let hottestStarTemp = 0;
 
@@ -34,6 +54,46 @@ function getPlanetWithHottestStar(planetData) {
     return _.find(planetData, function (planet) {
         return planet.HostStarTempK === hottestStarTemp;
     });
+}
+
+/**
+ *
+ * @param planetData
+ */
+function getPlanetTimeline(planetData) {
+    buildTimeline(planetData);
+    return _.orderBy(planetTimeline, ['yearDiscovered'], ['asc']);
+}
+
+function buildTimeline(planetData) {
+    _.forEach(planetData, function (planet) {
+        if (!planetTimeline.some(group => group.yearDiscovered === planet.DiscoveryYear)) {
+            addGroup(planet);
+        }
+        addSize(planet)
+    });
+}
+
+function addGroup(planet) {
+    planetTimeline.push({
+        yearDiscovered: planet.DiscoveryYear,
+        small: 0,
+        medium: 0,
+        large: 0
+    })
+}
+
+function addSize(planet) {
+    const planetSize = getPlanetSize(planet);
+    _.find(planetTimeline, function (group) {
+        return group.yearDiscovered === planet.DiscoveryYear;
+    })[planetSize]++;
+}
+
+function getPlanetSize(planet) {
+    if (planet.RadiusJpt < 1) return 'small';
+    if (planet.RadiusJpt < 2) return 'medium';
+    return 'large';
 }
 
 module.exports = {
