ae5a2e8d93e9af88662a014eb292dcfe80d44bfd
Freddy Kudelka <freddy.kudelka@aa.com>
2019-06-27 11:26:30 -0700

    Add error handling

--- src/routes/exoplanets.js
+++ src/routes/exoplanets.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const exoplanetService = require('../service/exoplanets.js');
-const request = require('request');
-
+const request = require('request-promise');
+const exoplanetsService = require('../service/exoplanets.js');
 const exoplanetsUrl = 'https://gist.githubusercontent.com/joelbirchler/66cf8045fcbb6515557347c05d789b4a/raw/9a196385b44d4288431eef74896c0512bad3defe/exoplanets';
 
 /* GET filtered catalogue of discovered exoplanets. */
 router.get('/', function (req, res) {
-    request.get(exoplanetsUrl, function (err, resp, body) {
-        const filteredData = exoplanetService.getFilteredExoplanetData(JSON.parse(body));
-        res.json(filteredData);
-    });
+    request.get(exoplanetsUrl)
+        .then(function (res) {
+            const body = JSON.parse(res);
+            res.json(exoplanetsService.getFilteredExoplanetData(body));
+        })
+        .catch(function (err) {
+            res.send(err, err.statusCode)
+        });
+
 });
 
 module.exports = router;
